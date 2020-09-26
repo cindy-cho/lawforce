e5660e4b85d67cd76caf28b06e927ae42f659434
HOXYMOLA <50163360+HOXYMOLA@users.noreply.github.com>
2020-09-26 17:38:48 +0900

    Update App.js

--- hoxymola/myNewProject/App.js
+++ hoxymola/myNewProject/App.js
@@ -9,9 +9,7 @@ export default function App() {
         <Text style={styles.boldText}>"Hello World!"</Text>
       </View>
       <View style={styles.body}>
-        <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
-        <Text style={styles.normalText}>Lorem ipsum dolor sit amet.</Text>
-        <Text style={styles.normalText}>Lorem ipsum dolor sit amet.</Text>
+        <Text style={styles.normalText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -38,8 +36,8 @@ const styles = StyleSheet.create({
   },
   normalText: {
     fontWeight: 'normal',
+    fontSize: 10,
     textAlign: 'center',
-    textTransform: 'uppercase',
   },
   body: {
     backgroundColor: 'yellow',
