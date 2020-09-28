066ad0d8006cb73dcb50307c5b30a863549765cd
HOXYMOLA <50163360+HOXYMOLA@users.noreply.github.com>
2020-09-28 18:31:21 +0900

    button

--- hoxymola/myNewProject/App.js
+++ hoxymola/myNewProject/App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [state, changeState] = useState({ first: '?????', second: '?????' });
+
+  const clickHandler = () => {
+    changeState({ first: 'Hello', second: 'World' });
+  }
+  const clickHandler2 = () => {
+    changeState({ first: '?????', second: '?????' });
+  }
+  
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>"Hello World!"</Text>
+      <Text style={styles.bigText}>{state.first}, {state.second}!</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+        <Button title='initialize' onPress={clickHandler2} />
       </View>
-      <View style={styles.body}>
-        <Text style={styles.normalText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-      </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bigText: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
