c610d137faa50792cf673557dd8cdab0117297ac
rb8925 <audrb8925@naver.com>
2020-10-13 23:32:20 +0900

    main_menu
    main

---
+++ 한덕규_to_do_list/todolist/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Header from './Header'
+import Body from './Body'
+
+export default class App extends React.Component {
+  state ={
+    todos:[]
+  }
+   addTodo = (todo) => {
+     
+    const newTodo = {
+        id: Date.now(), // 등록시간
+        text: todo,      // 할일 내용
+        completed: false, // 완료 여부
+    }   
+
+    // state 업데이트
+    this.setState(prevState => ({
+        todos: [
+            newTodo,       // 새로 추가된 할일(todo)
+            ...prevState.todos // 기존의 할일 목록
+        ]
+    }));
+   
+    // 콘솔창에 출력
+    console.log(this.state.todos);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>노동법 총정리!</Text>
+        <Text style={styles.marginBottom}>made by HanDeokGyu</Text>
+        <Header />
+        <Body />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: "#EEE",
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 30, 
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  marginBottom:{
+    fontWeight:"700",
+    fontSize:15,
+    marginBottom: 20,
+    marginLeft:220,
+  }
+});
---
+++ 한덕규_to_do_list/todolist/Header.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+class Header extends Component {
+    state ={
+        newTodo: '',
+    }
+    addNewTodo = () => {
+
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.input}> 
+                    <TextInput 
+                        style={styles.inputText}
+                        placeholder='Selection Menu!!'
+                        
+                    />
+                    
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    input: {
+        borderRadius: 10,
+        backgroundColor: "#FFF",
+        paddingLeft: 10,
+        paddingRight: 10,
+        height: 50,
+        alignItems: "center",
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    inputText: {
+        flex: 1,
+    },
+    addBtn: {
+        color: '#4169E1'
+    }
+});
+
+export default Header;
