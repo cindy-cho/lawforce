c92c4aaf68dc80a6ad2eedd61ee5903b7da1ef87
HOXYMOLA <50163360+HOXYMOLA@users.noreply.github.com>
2020-12-02 21:51:17 +0900

    Revert "key값 수정"
    This reverts commit 30b33f47d8f0a37173081b2ed65fbb5143cab078.

--- hoxymola/APP/tabbar/data/GBJ_GRGGB.js
+++ hoxymola/APP/tabbar/data/GBJ_GRGGB.js
@@ -1,246 +1,246 @@
 export const GBJ_GRGGB = [
     { 
-        key: '1',
+        key: '10',
         title: '근로계약 당사자의 권리·의무',
         content: [
         ]
     },
     { 
-        key: '2',
+        key: '11',
         title: '근로자의 취업청구권',
         content: [
         ]
     },
     { 
-        key: '3',
+        key: '12',
         title: '근로계약의 효력',
         content: [
         ]
     },
     { 
-        key: '4',
+        key: '13',
         title: '근로계약서의 작성 및 교부의무',
         content: [
         ]
     },
     { 
-        key: '5',
+        key: '14',
         title: '취업규칙의 작성·불의익하지 않은 변경',
         content: [
         ]
     },
     { 
-        key: '6',
+        key: '15',
         title: '취업규칙 불이익변경',
         content: [
         ]
     },
     { 
-        key: '7',
+        key: '16',
         title: '불이익 변경절차(동의)를 얻지 못한\n취업규칙의 효력',
         content: [
         ]
     },
     { 
-        key: '8',
+        key: '17',
         title: '차별 금지(근로기준법·남녀고용평등법)',
         content: [
         ]
     },
     { 
-        key: '9',
+        key: '18',
         title: '직장 내 성희롱·괴롭힘 금지',
         content: [
         ]
     },
     { 
-        key: '10',
+        key: '19',
         title: '근로기준법 기본원칙',
         content: [
         ]
     },
     { 
-        key: '11',
+        key: '20',
         title: '위약예정의 금지',
         content: [
         ]
     },
     { 
-        key: '12',
+        key: '21',
         title: '전차금 상계금지·강제 저금의 금지',
         content: [
         ]
     },
     { 
-        key: '13',
+        key: '22',
         title: '과도적 근로관계(채용내정·시용)',
         content: [
         ]
     },
     { 
-        key: '14',
+        key: '23',
         title: '임금의 개념',
         content: [
         ]
     },
     { 
-        key: '15',
+        key: '24',
         title: '평균임금',
         content: [
         ]
     },
     { 
-        key: '16',
+        key: '25',
         title: '통상임금',
         content: [
         ]
     },
     { 
-        key: '17',
+        key: '26',
         title: '휴업수당',
         content: [
         ]
     },
     { 
-        key: '18',
+        key: '27',
         title: '최저임금',
         content: [
         ]
     },
     { 
-        key: '19',
+        key: '28',
         title: '임금지급의 방법',
         content: [
         ]
     },
     { 
-        key: '20',
+        key: '29',
         title: '근로시간의 개념 및 시간',
         content: [
         ]
     },
     { 
-        key: '21',
+        key: '30',
         title: '휴게시간',
         content: [
         ]
     },
     { 
-        key: '22',
+        key: '31',
         title: '휴일, 주휴일',
         content: [
         ]
     },
     { 
-        key: '23',
+        key: '32',
         title: '연장·야간·휴일근로와 가산임금',
         content: [
         ]
     },
     { 
-        key: '24',
+        key: '33',
         title: '포괄임금계약',
         content: [
         ]
     },
     { 
-        key: '25',
+        key: '34',
         title: '연차휴가',
         content: [
         ]
     },
     { 
-        key: '26',
+        key: '35',
         title: '인사와 징계',
         content: [
         ]
     },
     { 
-        key: '27',
+        key: '36',
         title: '전직',
         content: [
         ]
     },
     { 
-        key: '28',
+        key: '37',
         title: '전적',
         content: [
         ]
     },
     { 
-        key: '29',
+        key: '38',
         title: '휴직',
         content: [
         ]
     },
     { 
-        key: '30',
+        key: '39',
         title: '직위해제',
         content: [
         ]
     },
     { 
-        key: '31',
+        key: '40',
         title: '징계',
         content: [
         ]
     },
     { 
-        key: '32',
+        key: '41',
         title: '근로자 측 사정에 따른 해고의 정당성\n(근기법 제 23조 제1항 정당한 이유)',
         content: [
         ]
     },
     { 
-        key: '33',
+        key: '42',
         title: '정리해고(경영해고)',
         content: [
         ]
     },
     { 
-        key: '34',
+        key: '43',
         title: '전리해고와 우선재고용',
         content: [
         ]
     },
     { 
-        key: '35',
+        key: '44',
         title: '해고금지기간',
         content: [
         ]
     },
     { 
-        key: '36',
+        key: '45',
         title: '해고의 법정절차',
         content: [
         ]
     },
     { 
-        key: '37',
+        key: '46',
         title: '부당해고 등의 구제',
         content: [
         ]
     },
     { 
-        key: '38',
+        key: '47',
         title: '근로관계의 종료사유',
         content: [
         ]
     },
     { 
-        key: '39',
+        key: '48',
         title: '근로관계 종료에 따른 법률관계\n(퇴직급여제도)',
         content: [
         ]
     },
     { 
-        key: '40',
+        key: '49',
         title: '근로관계 종료에 따른 법률관',
         content: [
         ]
     },
     {
-        key: '41',
+        key: '50',
         title: '사업변동과 노동관계',
         content: [
         ]
--- hoxymola/APP/tabbar/data/JN.js
+++ hoxymola/APP/tabbar/data/JN.js
@@ -1,206 +1,206 @@
 export const JN = [
     {
-        key: '1',
+        key: '58',
         title: '근로3권',
         content: []    
     },
     {
-        key: '2',
+        key: '59',
         title: '단결권(단결강제·소극적 단결권)',
         content: []    
     },
     {
-        key: '3',
+        key: '60',
         title: '노동조합의 설립요건',
         content: []    
     },
     {
-        key: '4',
+        key: '61',
         title: '노조법 제2조 제4호 라목"에 대한 논의',
         content: []    
     },
     {
-        key: '5',
+        key: '62',
         title: '법외조합(근로자단체)의 법적 지위',
         content: []    
     },
     {
-        key: '6',
+        key: '63',
         title: '노동조합의 민주적·자주적 운영',
         content: []    
     },
     {
-        key: '7',
+        key: '64',
         title: '노동조합 기관',
         content: []    
     },
     {
-        key: '8',
+        key: '65',
         title: '조합비 공제제도(check-off)',
         content: []    
     },
     {
-        key: '9',
+        key: '66',
         title: '노조전임자',
         content: []    
     },
     {
-        key: '10',
+        key: '67',
         title: '노동조합의 통제권',
         content: []    
     },
     {
-        key: '11',
+        key: '68',
         title: '노동조합의 조직변동',
         content: []    
     },
     {
-        key: '12',
+        key: '69',
         title: '조직협태의 변경방법',
         content: []    
     },
     {
-        key: '13',
+        key: '70',
         title: '조합활동의 정당성',
         content: []    
     },
     {
-        key: '14',
+        key: '71',
         title: '단체교섭의 당사자',
         content: []    
     },
     {
-        key: '15',
+        key: '72',
         title: '사업(장)단위의 교섭창구 단일화',
         content: []    
     },
     {
-        key: '16',
+        key: '73',
         title: '공정대표의무',
         content: []    
     },
     {
-        key: '17',
+        key: '74',
         title: '단체교섭의 담당자',
         content: []    
     },
     {
-        key: '18',
+        key: '75',
         title: '단체협약 체결권과 노동조합의 인준권',
         content: []    
     },
     {
-        key: '19',
+        key: '76',
         title: '단체교섭의 대상',
         content: []    
     },
     {
-        key: '20',
+        key: '77',
         title: '단체교섭의 방법과 그 위반에 대한 구제',
         content: []    
     },
     {
-        key: '21',
+        key: '78',
         title: '단체협약의 의의와 성립',
         content: []    
     },
     {
-        key: '22',
+        key: '79',
         title: '단체협약의 효력',
         content: []    
     },
     {
-        key: '23',
+        key: '80',
         title: '단체협약의 문제 조항',
         content: []    
     },
     {
-        key: '24',
+        key: '81',
         title: '단체협약의 해석',
         content: []    
     },
     {
-        key: '25',
+        key: '82',
         title: '단체협약의 효력확장제도',
         content: []    
     },
     {
-        key: '26',
+        key: '83',
         title: '단체협약의 실효',
         content: []    
     },
     {
-        key: '27',
+        key: '84',
         title: '노동쟁의의 개념(조정의 대상)',
         content: []    
     },
     {
-        key: '28',
+        key: '85',
         title: '쟁의행위의 개념과 준법투쟁',
         content: []    
     },
     {
-        key: '29',
+        key: '86',
         title: '쟁의행위에 대한 보호',
         content: []    
     },
     {
-        key: '30',
+        key: '87',
         title: '쟁의행위 방법상 제한·금지법규',
         content: []    
     },
     {
-        key: '31',
+        key: '88',
         title: '쟁의행위의 정당성',
         content: []    
     },
     {
-        key: '32',
+        key: '89',
         title: '위법한 쟁의행위와 책임 귀속',
         content: []    
     },
     {
-        key: '33',
+        key: '90',
         title: '쟁의행위와 근로계약관계',
         content: []    
     },
     {
-        key: '34',
+        key: '91',
         title: '직장폐쇄',
         content: []    
     },
     {
-        key: '35',
+        key: '92',
         title: '부당노동행위 총론',
         content: []    
     },
     {
-        key: '36',
+        key: '93',
         title: '불이익취급',
         content: []    
     },
     {
-        key: '37',
+        key: '94',
         title: '반조합계약(비열계약)',
         content: []    
     },
     {
-        key: '38',
+        key: '95',
         title: '지배·개입',
         content: []    
     },
     {
-        key: '39',
+        key: '96',
         title: '부당노동행위 구제',
         content: []    
     },
     {
-        key: '40',
+        key: '97',
         title: '공무원의 노동조합 설립 및 운영',
         content: []    
     },
     {
-        key: '41',
+        key: '98',
         title: '교원의 노동조합 설립 및 운영',
         content: []
     }
