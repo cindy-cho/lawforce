30b33f47d8f0a37173081b2ed65fbb5143cab078
rb8925 <audrb8925@naver.com>
2020-12-02 16:07:22 +0900

    key값 수정
    key 각 파트별 초기화

--- hoxymola/APP/tabbar/data/JN.js
+++ hoxymola/APP/tabbar/data/JN.js
@@ -1,206 +1,206 @@
 export const JN = [
     {
-        key: '58',
+        key: '1',
         title: '근로3권',
         content: []    
     },
     {
-        key: '59',
+        key: '2',
         title: '단결권(단결강제·소극적 단결권)',
         content: []    
     },
     {
-        key: '60',
+        key: '3',
         title: '노동조합의 설립요건',
         content: []    
     },
     {
-        key: '61',
+        key: '4',
         title: '노조법 제2조 제4호 라목"에 대한 논의',
         content: []    
     },
     {
-        key: '62',
+        key: '5',
         title: '법외조합(근로자단체)의 법적 지위',
         content: []    
     },
     {
-        key: '63',
+        key: '6',
         title: '노동조합의 민주적·자주적 운영',
         content: []    
     },
     {
-        key: '64',
+        key: '7',
         title: '노동조합 기관',
         content: []    
     },
     {
-        key: '65',
+        key: '8',
         title: '조합비 공제제도(check-off)',
         content: []    
     },
     {
-        key: '66',
+        key: '9',
         title: '노조전임자',
         content: []    
     },
     {
-        key: '67',
+        key: '10',
         title: '노동조합의 통제권',
         content: []    
     },
     {
-        key: '68',
+        key: '11',
         title: '노동조합의 조직변동',
         content: []    
     },
     {
-        key: '69',
+        key: '12',
         title: '조직협태의 변경방법',
         content: []    
     },
     {
-        key: '70',
+        key: '13',
         title: '조합활동의 정당성',
         content: []    
     },
     {
-        key: '71',
+        key: '14',
         title: '단체교섭의 당사자',
         content: []    
     },
     {
-        key: '72',
+        key: '15',
         title: '사업(장)단위의 교섭창구 단일화',
         content: []    
     },
     {
-        key: '73',
+        key: '16',
         title: '공정대표의무',
         content: []    
     },
     {
-        key: '74',
+        key: '17',
         title: '단체교섭의 담당자',
         content: []    
     },
     {
-        key: '75',
+        key: '18',
         title: '단체협약 체결권과 노동조합의 인준권',
         content: []    
     },
     {
-        key: '76',
+        key: '19',
         title: '단체교섭의 대상',
         content: []    
     },
     {
-        key: '77',
+        key: '20',
         title: '단체교섭의 방법과 그 위반에 대한 구제',
         content: []    
     },
     {
-        key: '78',
+        key: '21',
         title: '단체협약의 의의와 성립',
         content: []    
     },
     {
-        key: '79',
+        key: '22',
         title: '단체협약의 효력',
         content: []    
     },
     {
-        key: '80',
+        key: '23',
         title: '단체협약의 문제 조항',
         content: []    
     },
     {
-        key: '81',
+        key: '24',
         title: '단체협약의 해석',
         content: []    
     },
     {
-        key: '82',
+        key: '25',
         title: '단체협약의 효력확장제도',
         content: []    
     },
     {
-        key: '83',
+        key: '26',
         title: '단체협약의 실효',
         content: []    
     },
     {
-        key: '84',
+        key: '27',
         title: '노동쟁의의 개념(조정의 대상)',
         content: []    
     },
     {
-        key: '85',
+        key: '28',
         title: '쟁의행위의 개념과 준법투쟁',
         content: []    
     },
     {
-        key: '86',
+        key: '29',
         title: '쟁의행위에 대한 보호',
         content: []    
     },
     {
-        key: '87',
+        key: '30',
         title: '쟁의행위 방법상 제한·금지법규',
         content: []    
     },
     {
-        key: '88',
+        key: '31',
         title: '쟁의행위의 정당성',
         content: []    
     },
     {
-        key: '89',
+        key: '32',
         title: '위법한 쟁의행위와 책임 귀속',
         content: []    
     },
     {
-        key: '90',
+        key: '33',
         title: '쟁의행위와 근로계약관계',
         content: []    
     },
     {
-        key: '91',
+        key: '34',
         title: '직장폐쇄',
         content: []    
     },
     {
-        key: '92',
+        key: '35',
         title: '부당노동행위 총론',
         content: []    
     },
     {
-        key: '93',
+        key: '36',
         title: '불이익취급',
         content: []    
     },
     {
-        key: '94',
+        key: '37',
         title: '반조합계약(비열계약)',
         content: []    
     },
     {
-        key: '95',
+        key: '38',
         title: '지배·개입',
         content: []    
     },
     {
-        key: '96',
+        key: '39',
         title: '부당노동행위 구제',
         content: []    
     },
     {
-        key: '97',
+        key: '40',
         title: '공무원의 노동조합 설립 및 운영',
         content: []    
     },
     {
-        key: '98',
+        key: '41',
         title: '교원의 노동조합 설립 및 운영',
         content: []
     }
