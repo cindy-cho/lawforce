5afb2afff1c7fb66a965e136ab6a806943ce5919
HOXYMOLA <50163360+HOXYMOLA@users.noreply.github.com>
2020-12-28 10:51:20 +0900

    ddddd

--- hoxymola/APP/tabbar/screens/todaySample.js
+++ hoxymola/APP/tabbar/screens/todaySample.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import Global from '../shared/Global';
 import { BGGT } from '../data/BGGT';
+import { GBJ_GRGGB } from '../data/GBJ_GRGGB';
+import { JN } from '../data/JN';
+import { NDBCR } from '../data/NDBCR';
 
 function TodayElement({todayElement}){
+    var len = todayElement.content.length;
+    var idx = Math.floor(Math.random()*len);
+
     return(
         <View>
             <Text style={[Global.text, {color:'#D4D4D4',fontSize: 18, marginTop: 10}]}>
-                {todayElement.title}
+                {todayElement.content[idx].subtitle}
             </Text>
             <Text style={[Global.text, {marginTop:10, fontSize: 15, lineHeight : 27, color:'#D1D0CE'}]}>
-                {todayElement.content[0][0].text}
+                {todayElement.content[idx].text[0]}
             </Text>
         </View>
     );
@@ -18,11 +24,28 @@ function TodayElement({todayElement}){
 
 function Today () {
 
-    var idx = Math.floor(Math.random()*6);
+    // var TM;
+    // var idx1 = Math.floor(Math.random()*4);
+    // if(idx1 == 0){
+    //     var idx = Math.floor(Math.random()*BGGT.length);
+    //     var TM = BGGT[idx];
+    // }else if(idx1 == 1){
+    //     var idx = Math.floor(Math.random()*GBJ_GRGGB.length);
+    //     var TM = GBJ_GRGGB[idx];
+    // }else if(idx1 == 2){
+    //     var idx = Math.floor(Math.random()*JN.length);
+    //     var TM = JN[idx];
+    // }else{
+    //     var idx = Math.floor(Math.random()*NDBCR.length);
+    //     var TM = NDBCR[idx];
+    // }
+
+    var idx = Math.floor(Math.random()*NDBCR.length);
+    var TM = NDBCR[idx];
 
     return (
         <View style = {{flex:1, justifyContent : "center", alignItems :'center', paddingBottom: 10}}>
-            <TodayElement todayElement={BGGT[idx]} />
+            <TodayElement todayElement={TM}/>
         </View>
     );
 }
