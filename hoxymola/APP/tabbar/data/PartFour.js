26075bc456d7101210056b9b39982149cf3f26d7
cindy-cho <37435790+cindy-cho@users.noreply.github.com>
2021-01-03 08:51:38 +0900

    파트 4 인덱스 업데이트 (이제 컨텐츠만 채우면 됨)

--- hoxymola/APP/tabbar/data/PartFour.js
+++ hoxymola/APP/tabbar/data/PartFour.js
@@ -1,207 +1,722 @@
 export const ChapterFour = [
     {
-        key: '58',
-        title: '근로3권',
-        content: []    
-    },
-    {
-        key: '59',
-        title: '단결권(단결강제·소극적 단결권)',
-        content: []    
-    },
-    {
-        key: '60',
         title: '노동조합의 설립요건',
-        content: []    
+        key: '1',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '61',
         title: '노조법 제2조 제4호 라목"에 대한 논의',
-        content: []    
+        key: '2',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '62',
         title: '법외조합(근로자단체)의 법적 지위',
-        content: []    
+        key: '3',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '63',
         title: '노동조합의 민주적·자주적 운영',
-        content: []    
+        key: '4',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '64',
         title: '노동조합 기관',
-        content: []    
+        key: '5',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '65',
-        title: '조합비 공제제도(check-off)',
-        content: []    
-    },
-    {
-        key: '66',
         title: '노조전임자',
-        content: []    
+        key: '6',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '67',
-        title: '노동조합의 통제권',
-        content: []    
+        title: '조직형태의 변경방법',
+        key: '7',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '68',
-        title: '노동조합의 조직변동',
-        content: []    
-    },
-    {
-        key: '69',
-        title: '조직협태의 변경방법',
-        content: []    
-    },
-    {
-        key: '70',
         title: '조합활동의 정당성',
-        content: []    
+        key: '8',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '71',
         title: '단체교섭의 당사자',
-        content: []    
+        key: '9',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '72',
         title: '사업(장)단위의 교섭창구 단일화',
-        content: []    
+        key: '10',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '73',
         title: '공정대표의무',
-        content: []    
+        key: '11',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '74',
         title: '단체교섭의 담당자',
-        content: []    
+        key: '12',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '75',
         title: '단체협약 체결권과 노동조합의 인준권',
-        content: []    
+        key: '13',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '76',
         title: '단체교섭의 대상',
-        content: []    
+        key: '14',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '77',
         title: '단체교섭의 방법과 그 위반에 대한 구제',
-        content: []    
+        key: '15',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '78',
         title: '단체협약의 의의와 성립',
-        content: []    
+        key: '16',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '79',
         title: '단체협약의 효력',
-        content: []    
+        key: '17',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '80',
         title: '단체협약의 문제 조항',
-        content: []    
+        key: '18',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '81',
         title: '단체협약의 해석',
-        content: []    
+        key: '19',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '82',
         title: '단체협약의 효력확장제도',
-        content: []    
+        key: '20',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '83',
         title: '단체협약의 실효',
-        content: []    
+        key: '21',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '84',
         title: '노동쟁의의 개념(조정의 대상)',
-        content: []    
+        key: '22',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '85',
         title: '쟁의행위의 개념과 준법투쟁',
-        content: []    
+        key: '23',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '86',
         title: '쟁의행위에 대한 보호',
-        content: []    
+        key: '24',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '87',
         title: '쟁의행위 방법상 제한·금지법규',
-        content: []    
+        key: '25',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '88',
         title: '쟁의행위의 정당성',
-        content: []    
+        key: '26',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '89',
         title: '위법한 쟁의행위와 책임 귀속',
-        content: []    
+        key: '27',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '90',
         title: '쟁의행위와 근로계약관계',
-        content: []    
+        key: '28',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '91',
         title: '직장폐쇄',
-        content: []    
+        key: '29',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '92',
         title: '부당노동행위 총론',
-        content: []    
+        key: '30',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '93',
         title: '불이익취급',
-        content: []    
+        key: '31',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '94',
         title: '반조합계약(비열계약)',
-        content: []    
+        key: '32',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '95',
         title: '지배·개입',
-        content: []    
+        key: '33',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '96',
         title: '부당노동행위 구제',
-        content: []    
+        key: '34',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '97',
         title: '공무원의 노동조합 설립 및 운영',
-        content: []    
+        key: '35',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     },
     {
-        key: '98',
         title: '교원의 노동조합 설립 및 운영',
-        content: []
+        key: '36',
+        content: [
+            {
+                subtitle: '' ,
+                text: [ 
+                    '',
+                    ''
+                ]
+            },
+            {
+                subtitle : '',
+                text: [
+                    '',
+                    ''
+                ]
+            }
+        ]
     }
 ]
